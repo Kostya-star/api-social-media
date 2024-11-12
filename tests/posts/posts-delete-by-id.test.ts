ee363484e0b2d1c4961f857af0757e83edfd06b0
Kostya-star <konstantindanilov19@hotmail.com>
2024-11-12 10:31:17 +0200

    add posts delete vy id test

---
+++ tests/posts/posts-delete-by-id.test.ts
@@ -0,0 +1,60 @@
+import { HTTP_STATUS_CODES } from '../../src/settings/http-status-codes';
+import { createTestBlog, deleteTestBlog, getCreateBlogPayload } from '../blogs/helpers';
+import { IErrorItem } from '../../src/types/error-item';
+import { createTestPost, deleteTestPost, getCreatePostPayload, getTestPostById } from './helpers';
+import { PostsErrorsList } from '../../src/errors/posts-errors';
+
+describe('POSTS DELETE BY ID request', () => {
+  let testBlogId: string | null = null;
+  let testPostId: string | null = null;
+
+  beforeAll(async () => {
+    const blog = await createTestBlog(getCreateBlogPayload({}), true);
+    testBlogId = blog.body.id;
+  });
+
+  afterAll(async () => {
+    await deleteTestBlog(testBlogId!, true);
+    testBlogId = null;
+  });
+  
+  beforeEach(async () => {
+    const post = await createTestPost(getCreatePostPayload(testBlogId!)({}), true);
+    testPostId = post.body.id;
+  });
+  
+  afterEach(async () => {
+    if(testPostId) {
+      await deleteTestPost(testPostId!, true);
+      testPostId = null;
+    }
+  });
+
+  test('status check. should be 204', async () => {
+    const res = await deleteTestPost(testPostId!, true);
+    testPostId = null;
+    expect(res.status).toBe(HTTP_STATUS_CODES.NO_CONTENT_204);
+  });
+  test('status check. should be 401', async () => {
+    const res = await deleteTestPost(testPostId!, false);
+
+    expect(res.status).toBe(HTTP_STATUS_CODES.UNAUTHORIZED_401);
+  });
+  test('status check. should be 404 coz no post found', async () => {
+    const res = await deleteTestPost('12345678utygewxr2e12e12211221qw', true);
+
+    expect(res.status).toBe(HTTP_STATUS_CODES.NOT_FOUND_404);
+    const error: IErrorItem = { status: HTTP_STATUS_CODES.NOT_FOUND_404, message: PostsErrorsList.NOT_FOUND };
+    expect(res.body).toEqual(error);
+  });
+  test('successful deletion. post is deleted from DB and cant be found anymore', async () => {
+    const res =  await deleteTestPost(testPostId!, true);
+    
+    expect(res.status).toBe(HTTP_STATUS_CODES.NO_CONTENT_204);
+    
+    const post = await getTestPostById(testPostId!);
+    const error: IErrorItem = { status: HTTP_STATUS_CODES.NOT_FOUND_404, message: PostsErrorsList.NOT_FOUND };
+    expect(post.body).toEqual(error);
+    testPostId = null;
+  });
+});
