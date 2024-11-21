ea8f0c3f60a101a71fc799c6a2cd494cdf61bd70
Kostya-star <konstantindanilov19@hotmail.com>
2024-11-21 19:14:45 +0200

    fix get req for users

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -11,19 +11,21 @@ const getAllUsers = async (query: Required<GetAllUsersQuery>): Promise<IBaseResp
   const { searchEmailTerm, searchLoginTerm, pageNumber, pageSize, sortBy, sortDirection } = query;
   const { sortOptions, skip, limit } = buildQuery<IUser>({ pageNumber, pageSize, sortBy, sortDirection });
 
-  const searchConditions: Partial<Record<'login' | 'email', any>> = {};
+  const searchConditions = [];
 
   if (searchLoginTerm) {
-    searchConditions.login = { $regex: searchLoginTerm, $options: 'i' };
+    searchConditions.push({ login: { $regex: searchLoginTerm, $options: 'i' } });
   }
 
   if (searchEmailTerm) {
-    searchConditions.email = { $regex: searchEmailTerm, $options: 'i' };
+    searchConditions.push({ email: { $regex: searchEmailTerm, $options: 'i' } });
   }
 
-  const users = await usersCollection.find(searchConditions).sort(sortOptions).skip(skip).limit(limit).toArray();
+  const queryFilter = searchConditions.length ? { $or: searchConditions } : {};
 
-  const totalCount = await usersCollection.countDocuments(searchConditions);
+  const users = await usersCollection.find(queryFilter).sort(sortOptions).skip(skip).limit(limit).toArray();
+
+  const totalCount = await usersCollection.countDocuments(queryFilter);
   const pagesCount = Math.ceil(totalCount / pageSize);
 
   return {
@@ -44,6 +46,8 @@ const deleteUser = async (userId: ObjectId): Promise<void> => {
   await usersCollection.deleteOne({ _id: new ObjectId(userId) });
 };
 
+
+// IMPORTANT! This function isnt mapping the returned data, so you cant send out (res.json()) the data that this func returns
 const findUserByFilter = async (filter: Filter<IUser>): Promise<WithId<IUser> | null> => {
   const user = await usersCollection.findOne(filter);
   return user;
