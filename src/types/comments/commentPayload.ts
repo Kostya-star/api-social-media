428e5f4fa724f04509dbcde3e7d2e6b07f88f909
Kostya-star <konstantindanilov19@hotmail.com>
2024-12-07 01:09:51 +0200

    fix comments create

--- src/types/comments/commentPayload.ts
+++ src/types/comments/commentPayload.ts
@@ -1,5 +1,5 @@
 import { ICommentView } from './comment';
 
-export interface ICommentPayload extends Omit<ICommentView, 'id' | 'createdAt'> {
+export interface ICommentPayload extends Omit<ICommentView, 'id' | 'createdAt' | 'likesInfo'> {
   content: string;
 }
